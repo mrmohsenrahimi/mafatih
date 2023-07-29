f8014f98c2651f17f38618332fde205f2f7693cb
Mohsen Rahimi <mohsen.rahimi.sh1@gmail.com>
2023-07-29 14:02:41 +0330

    acccccrdion khareeeeee

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -19,3 +19,13 @@ function backColor() {
 
 filterIcon.addEventListener("click", displayFilter)
 inputSearch.addEventListener("focus", backColor)
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+    });
+}
