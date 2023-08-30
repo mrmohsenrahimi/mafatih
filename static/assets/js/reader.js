bb907a96608568e93313cffad4e42b7c243a6dd4
Mohsen Rahimi <mohsen.rahimi.sh1@gmail.com>
2023-08-30 12:27:04 +0330

    before change the indentation of code

--- static/assets/js/reader.js
+++ static/assets/js/reader.js
@@ -1,14 +1,43 @@
 /*SETTING BUTTON*/
 const settingBtn = document.querySelector(".setting-btn");
-const btnClose = document.querySelector(".offcanvas-bottom > .offcanvas-header > .btn-close");
 const header = document.querySelector(".navbar")
 
 settingBtn.addEventListener("click", function () {
     settingBtn.classList.toggle("setting-select");
     header.classList.toggle("z-1000");
 });
-
-btnClose.addEventListener("mousedown", function () {
-    settingBtn.classList.remove("setting-select");
-});
 /*SETTING BUTTON*/
+
+
+/*LINE HEIGHT BUTTONS*/
+const decreaseBtn = document.querySelector(".decrease");
+const increaseBtn = document.querySelector(".increase");
+let lineHeight = document.getElementById("lineHeight");
+let value = 1;
+const changeLineHeight = document.querySelector(".text-to-change-line-height");
+
+function changeLH() {
+    if (value <= 1) {
+        value = 1;
+    }
+    lineHeight.value = value;
+    changeLineHeight.style.lineHeight = lineHeight.value;
+    lineHeight.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+        }
+    )
+}
+
+decreaseBtn.addEventListener("click", function () {
+    value--;
+    changeLH();
+});
+increaseBtn.addEventListener("click", function () {
+    value++;
+    changeLH();
+});
+
+lineHeight.addEventListener("change", changeLH);
+
+/*LINE HEIGHT BUTTONS*/
