24d49543ed79e2eb8e20b0ec5d533f5b497e5ef3
Mohsen Rahimi <mohsen.rahimi.sh1@gmail.com>
2023-09-02 16:35:05 +0330

    redear page without database is complete

--- static/assets/js/reader.js
+++ static/assets/js/reader.js
@@ -1,10 +1,46 @@
-/*SETTING BUTTON*/
+/*FOOTER BUTTONS*/
 const settingBtn = document.querySelector(".setting-btn");
 const header = document.querySelector(".navbar");
+const speedBtn = document.querySelector(".speed-btn");
+const singerBtn = document.querySelector(".singer-btn");
+const offcanvasBottomSetting = document.querySelector(".offcanvas-setting");
+const offcanvasBottomSpeed = document.querySelector(".offcanvas-speed");
+const offcanvasBottomSinger = document.querySelector(".offcanvas-singer");
 
 settingBtn.addEventListener("click", function () {
-	settingBtn.classList.toggle("setting-select");
-	header.classList.toggle("z-1000");
+	settingBtn.classList.toggle("footer-buttons-select");
+	speedBtn.classList.remove("footer-buttons-select");
+	singerBtn.classList.remove("footer-buttons-select");
+	offcanvasBottomSetting.classList.toggle("show");
+	if (offcanvasBottomSetting.classList.contains("show")) {
+		header.classList.add("z-1000");
+	} else if (!offcanvasBottomSetting.classList.contains("show")) {
+		header.classList.remove("z-1000");
+	}
+});
+
+speedBtn.addEventListener("click", function () {
+	speedBtn.classList.toggle("footer-buttons-select");
+	settingBtn.classList.remove("footer-buttons-select");
+	singerBtn.classList.remove("footer-buttons-select");
+	offcanvasBottomSpeed.classList.toggle("show");
+	if (offcanvasBottomSpeed.classList.contains("show")) {
+		header.classList.add("z-1000");
+	} else if (!offcanvasBottomSpeed.classList.contains("show")) {
+		header.classList.remove("z-1000");
+	}
+});
+
+singerBtn.addEventListener("click", function () {
+	singerBtn.classList.toggle("footer-buttons-select");
+	settingBtn.classList.remove("footer-buttons-select");
+	speedBtn.classList.remove("footer-buttons-select");
+	offcanvasBottomSinger.classList.toggle("show");
+	if (offcanvasBottomSinger.classList.contains("show")) {
+		header.classList.add("z-1000");
+	} else if (!offcanvasBottomSinger.classList.contains("show")) {
+		header.classList.remove("z-1000");
+	}
 });
 /*SETTING BUTTON*/
 
@@ -37,3 +73,20 @@ increaseBtn.addEventListener("click", function () {
 
 document.getElementById("fontSize").value = defaultFontSize;
 /*FONT SIZE BUTTONS*/
+
+/* SINGER SELECTION */
+const singers = document.querySelectorAll(".singer-box");
+
+function toggleActiveSinger(item) {
+	singers.forEach(item => {
+		item.classList.remove("singer-box-active");
+	});
+	item.classList.add("singer-box-active");
+}
+
+singers.forEach(function (item) {
+	item.addEventListener("click", function (e) {
+		toggleActiveSinger(e.target.closest(".singer-box"));
+	});
+});
+/* SINGER SELECTION */
